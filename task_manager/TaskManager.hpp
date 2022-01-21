93b9b0d3d8997005313b6c84a10efde0e831daad
Shynn Lawrence <shynn.lawrence@gmail.com>
2022-01-21 21:04:12 +0000

    Shifting to source file compilation instead of .h

--- task_manager/TaskManager.hpp
+++ task_manager/TaskManager.hpp
@@ -9,23 +9,12 @@ class TimeObj{
     TimeObj(unsigned long long time){
         this->time = time;
     }
-    std::string to_string(){
-        unsigned long long tempTime = time;
-        int years = tempTime / 31556952000;
-        tempTime %= 31556952000;
-        int days = tempTime / 86400000;
-        tempTime %= 86400000;
-        int hours = tempTime / 3600000;
-        tempTime %= 3600000;
-        int minutes = tempTime / 60000;
-        tempTime %= 60000;
-        int seconds = tempTime / 1000;
-        tempTime %= 1000;
-        std::string time = "";
-        time = time + std::to_string(years) + " years, " + 
-            std::to_string(days) + " days, " + 
-            std::to_string(hours) + " hours, and " + 
-            std::to_string(minutes) + " minutes"; 
-        return time;
+    std::string to_string();
+    TimeObj operator+(TimeObj input);
+    TimeObj operator-(TimeObj input);
+
+    static TimeObj currentTimeObj(){
+        return TimeObj(std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()).count());
     }
 };
