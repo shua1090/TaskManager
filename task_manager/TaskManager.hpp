9b735cd06a08ccfd38f84e2e276ad276adc0180c
shua1090 <shynn.lawrence@gmail.com>
2022-01-24 11:54:44 -0800

    TaskManager class, time literals

--- task_manager/TaskManager.hpp
+++ task_manager/TaskManager.hpp
@@ -1,20 +1,120 @@
-#include <chrono>
-#include <string>
+#include "Time.h"
 
-class TimeObj{
-    unsigned long long time;
-    // Milliseconds in a year: 31,556,952,000
-    
-    public:
-    TimeObj(unsigned long long time){
-        this->time = time;
+#ifndef TASK_MANAGER
+#define TASK_MANAGER
+
+#include <vector>
+#include <thread>
+#include <functional>
+#include <mutex>
+#include <queue>
+
+class Task{
+    TimeObj timeToExecute = {0};
+    TimeObj repeatTime = {0};
+    std::function<void()> function;
+public:
+    Task()= default;
+    Task( const Task& other){
+        timeToExecute = other.timeToExecute;
+        repeatTime = other.repeatTime;
+        function = other.function;
     }
-    std::string to_string();
-    TimeObj operator+(TimeObj input);
-    TimeObj operator-(TimeObj input);
 
-    static TimeObj currentTimeObj(){
-        return TimeObj(std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now().time_since_epoch()).count());
+    template<typename F>
+    void setTask(std::function<F> f){
+        this->function = f;
+    }
+    void setTime(const TimeObj& time){
+        this->timeToExecute = time;
+    }
+    void addToCurrentTime(const TimeObj& time){
+        this->timeToExecute = TimeObj::currentTimeObj() + time;
+    }
+    void repeatEvery(const TimeObj& time){
+        this->repeatTime = time;
+    }
+
+    [[nodiscard]] long long time() const{
+        return this->timeToExecute.get_time();
+    }
+
+    long long getrepeattime(){
+        return this->repeatTime.get_time();
+    }
+
+    void run(bool join = false){
+        std::thread thread = std::thread(this->function);
+        if (join){
+            thread.join();
+        } else {
+            thread.detach();
+        }
+    }
+
+};
+
+class Compare
+{
+public:
+    bool compare(Task a, Task b){
+        return a.time() > b.time();
+    }
+    bool operator() (Task A, Task B)
+    {
+        return compare(A, B);
     }
 };
+
+class TaskManager {
+
+    std::priority_queue<Task, std::vector<Task>, Compare > taskList;
+    std::mutex mutex;
+public:
+    volatile bool flag;
+    TaskManager()= default;
+
+    void addTask(Task task){
+        this->mutex.lock();
+        this->taskList.push(task);
+        this->mutex.unlock();
+    }
+
+    void taskLoop(){
+        this->mutex.lock();
+        Task t; t = this->taskList.top();
+        this->mutex.unlock();
+        auto curTime = TimeObj::currentTimeObj();
+        flag=true;
+        while(flag){
+            if (curTime > t.time()){
+
+                t.run();
+                this->mutex.lock();
+                if (t.getrepeattime() > 0){
+                    Task tempT(t);
+                    tempT.addToCurrentTime(tempT.getrepeattime());
+                    this->taskList.push(tempT);
+                }
+
+                this->taskList.pop();
+                if (!this->taskList.empty()) t = this->taskList.top();
+                else{
+                    while (true){
+                        this->mutex.lock();
+                        if(!this->taskList.empty()){
+                            break;
+                        }
+                        this->mutex.unlock();
+                    }
+                }
+                this->mutex.unlock();
+
+            }
+
+            curTime = TimeObj::currentTimeObj();
+        }
+    }
+};
+
+#endif //TASK_MANAGER
--- task_manager/main.cpp
+++ task_manager/main.cpp
@@ -1,6 +1,34 @@
 
 #include <iostream>
+#include "TaskManager.hpp"
+
+int toRun(){
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+
+int toRun2(){
+    std::cout << "Hello World 2!" << std::endl;
+    return 0;
+}
 
 int main(){
+    Task task;
+    task.setTask(std::function<int()>(toRun));
+    task.addToCurrentTime(5_s);
+    task.repeatEvery(5_s);
+
+    Task task2;
+    task2.setTask(std::function<int()>(toRun2));
+    task2.addToCurrentTime(6_s);
+    task2.repeatEvery(10_s);
+
+
+    TaskManager manager;
+    manager.addTask(task);
+    manager.addTask(task2);
+    manager.taskLoop();
+
+
     return 0;
 }
